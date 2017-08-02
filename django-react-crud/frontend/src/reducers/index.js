3df430b8c78b7cd18dccb0cc007deb87d3060f15
musale <musale@focusmobile.co>
2017-08-02 16:35:11 +0300

    Update(reducers): add login data reducer function

--- django-react-crud/frontend/src/reducers/index.js
+++ django-react-crud/frontend/src/reducers/index.js
@@ -5,14 +5,33 @@ const tokenInitialState = null;
 const token = (state = tokenInitialState, action) => {
   switch(action.type) {
     case actionType.SET_TOKEN:
-      return action.data;
+      return action.token;
     default:
       return state;
   }
 }
+const loginDataInitialState = {
+  username: '',
+  password: ''
+}
+
+const loginData = (state=loginDataInitialState, action) =>{
+  let {username, password} = state;
+  switch (action.type) {
+    case action.SET_USERNAME:
+      username = action.username;
+      break;
+    case action.SET_PASSWORD:
+      password = action.password;
+      break;
+    default:
+      return state
+  }
+  return {username, password}
+}
 
 const appReducer = combineReducers({
-  token,
+  token: token, loginData: loginData,
 })
 
 const rootReducer = (state, action) => {
