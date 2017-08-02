180b235cb0ba5c70863404503c5f555f1eb652a4
musale <musale@focusmobile.co>
2017-08-02 16:33:33 +0300

    Update(actions) setUsername and setPassword functions

--- django-react-crud/frontend/src/actions/index.js
+++ django-react-crud/frontend/src/actions/index.js
@@ -1,8 +1,22 @@
 import * as actionType from './types';
 
-export const setToken = (data) => {
+export const setToken = (token) => {
   return {
     type: actionType.SET_TOKEN,
-    data
+    token: token
+  }
+}
+
+export const setUsername = (username) => {
+  return {
+    type: actionType.SET_USERNAME,
+    username: username.trim()
+  }
+}
+
+export const setPassword = (password) => {
+  return {
+    type: actionType.SET_PASSWORD,
+    password: password.trim()
   }
 }
