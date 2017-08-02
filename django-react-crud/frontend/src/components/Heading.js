33e61ac476a92357592775ed90dda4a284d871ab
musale <musale@focusmobile.co>
2017-08-02 14:23:09 +0300

    Update(frontend): add redux for state management

---
+++ django-react-crud/frontend/src/components/Heading.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Heading = () => {
+  return (
+    <h3>Welcome to DREACT</h3>
+  )
+}
+
+export default Heading;
---
+++ django-react-crud/frontend/src/containers/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Heading from '../components/Heading';
+import '../styles/index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Heading/>
+      </div>
+    );
+  }
+}
+
+export default App;
