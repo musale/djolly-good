b1ee56f68ac2e969c7a305fb0c053b75aeb63d33
musale <musale@focusmobile.co>
2017-08-04 13:17:48 +0300

    Create(models): create Author model

--- graphcool/graphcool/graphapp/models.py
+++ graphcool/graphcool/graphapp/models.py
@@ -1,3 +1,16 @@
+"""Graph app models."""
+from __future__ import unicode_literals
 from django.db import models
 
-# Create your models here.
+
+class Author(models.Model):
+    """Author model."""
+
+    first_name = models.CharField(max_length=32)
+    last_name = models.CharField(max_length=32)
+    email = models.EmailField()
+    created_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        """Object string literal."""
+        return self.first_name
